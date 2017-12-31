9df396fa61e9c2de799b71ba4b2a404f8ae114ed
mrthaker <mahek.haker@gmail.com>
2017-12-31 08:16:57 +0000

    zuk: don't use common tree because it hate it

--- device.mk
+++ device.mk
@@ -97,7 +97,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
@@ -157,6 +159,10 @@ $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 # VNDK-SP:
 PRODUCT_PACKAGES += \
    vndk-sp
+
+# Lights
+PRODUCT_PACKAGES += \
+   lights.msm8996
 
 # HIDL manifest
 PRODUCT_COPY_FILES += \
@@ -246,7 +252,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.qcom.bt.sh \
     ueventd.qcom.rc \
-    init.qcom.post_boot.sh
+    init.qcom.post_boot.sh 
 
 # RIL
 PRODUCT_PACKAGES += \
